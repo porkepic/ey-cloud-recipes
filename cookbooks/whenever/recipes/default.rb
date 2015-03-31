156efaeae2c82f015ffdd2a450e9f57bb6ff23e7
Julien Guimont <julien.guimont@gmail.com>
2015-03-31 13:48:46 -0400

    Refresh whenever recipe

--- cookbooks/whenever/recipes/default.rb
+++ cookbooks/whenever/recipes/default.rb
@@ -11,19 +11,18 @@ if ['solo', 'util'].include?(node[:instance_role])
       message "starting whenever recipe for app '#{app}'"
     end
     # be sure to replace "app_name" with the name of your application.
-    local_user = node[:users].first
+    # local_user = node[:users].first
     execute "whenever" do
       cwd "/data/#{app}/current"
-      user local_user[:username]
-      command "bundle exec whenever -w -s 'environment=#{node[:environment][:framework_env]}'"
-      action :run
+      epic_fail true
+      command "bundle exec whenever --update-crontab '#{app}' 'environment=#{node[:environment][:framework_env]}'"
     end
-    
+
     ey_cloud_report "whenever" do
       message "whenever recipe complete for app '#{app}'"
     end
   end
-  
+
 end
 
 
